67471ed3a542acb581f190de050f44e35c3b1910
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-07-04 15:14:39 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -2,5 +2,6 @@ $ErrorActionPreference = "Stop"
 echo "Path to aws tools:"
 Write-Output (cmd /c where aws)
 if ($LastExitCode -ne 0) {$host.SetShouldExit($LastExitCode)}
-aws --version
+$version = aws --version
+echo "version"
 Get-Command Remove-EC2Instance
